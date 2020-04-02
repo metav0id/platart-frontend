7767c5fa211b731e53f3bc15c9f49fb1e0d126a2
Alan Schmollgruber <alanschmollgruber@gmail.com>
2020-04-02 14:23:33 +0200

    Experimental table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { WarehouseQueenComponent } from './content/warehouse-queen/warehouse-queen.component';
 import { StockInWarehouseComponent } from './content/warehouse-queen/stock-in-warehouse/stock-in-warehouse.component';
 import {HttpClientModule} from '@angular/common/http';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import {CdkTableModule} from '@angular/cdk/table';
 
 @NgModule({
   declarations: [
@@ -12,10 +14,12 @@ import {HttpClientModule} from '@angular/common/http';
     WarehouseQueenComponent,
     StockInWarehouseComponent
   ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        NoopAnimationsModule,
+        CdkTableModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
