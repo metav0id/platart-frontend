ab8b4f70ffcd8ce4b491d7872c01377c124c268b
Monica Hernandez Ascencio <monicaherasc@gmail.com>
2020-04-08 08:37:17 +0200

    Header and Footer added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,19 +8,41 @@ import {SalesPrincessComponent} from './content/sales-princess/sales-princess.co
 import {ManagerKingComponent} from './content/manager-king/manager-king.component';
 import {PageNotFoundComponent} from './content/page-not-found/page-not-found.component';
 import {WarehouseQueenModule} from './content/warehouse-queen/warehouse-queen.module';
+import {CdkTableModule} from '@angular/cdk/table';
+import {MatTableModule} from '@angular/material/table';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NewDeliveryToShopComponent} from './content/warehouse-queen/new-delivery-to-shop/new-delivery-to-shop.component';
+import {FormsModule} from '@angular/forms';
+import { HeaderComponent } from './content/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import {MatMenuModule} from "@angular/material/menu";
+import { FooterComponent } from './content/footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SalesPrincessComponent,
     ManagerKingComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     WarehouseQueenModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
