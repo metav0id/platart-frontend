84cc656094a49814e929213ddba83b0ca2fca70e
metav0id <volkerhauck@def-design.de>
2020-04-24 19:21:30 +0200

    bugfix transloco

---
+++ src/app/content/manager-king/management-dashboard/management-dashboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-management-dashboard',
+  templateUrl: './management-dashboard.component.html',
+  styleUrls: ['./management-dashboard.component.css']
+})
+export class ManagementDashboardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/content/manager-king/manager-king.module.ts
+++ src/app/content/manager-king/manager-king.module.ts
@@ -2,12 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {ManagerKingRoutingModule} from "./manager-king-routing.module";
 import { ManageShopsInfoComponent } from './manage-shops-info/manage-shops-info.component';
+import { ManagementDashboardComponent } from './management-dashboard/management-dashboard.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {AppComponent} from '../../app.component';
 
 @NgModule({
-  declarations: [ManageShopsInfoComponent],
+  declarations: [ManageShopsInfoComponent,
+    ManagementDashboardComponent],
   imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
     ManagerKingRoutingModule
-  ]
+  ],
+  bootstrap: [AppComponent]
 })
 export class ManagerKingModule { }
