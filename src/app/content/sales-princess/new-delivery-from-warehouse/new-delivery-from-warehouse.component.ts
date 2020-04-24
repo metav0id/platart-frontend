bbb738bb207ec2a1096c96ec0094c788a3fd5e53
metav0id <volkerhauck@def-design.de>
2020-04-24 19:26:28 +0200

    bugfix transloco

--- src/app/content/sales-princess/new-delivery-from-warehouse/new-delivery-from-warehouse.component.ts
+++ src/app/content/sales-princess/new-delivery-from-warehouse/new-delivery-from-warehouse.component.ts
@@ -1,4 +1,3 @@
-import { Component, OnInit } from '@angular/core';
 import {TRANSLOCO_SCOPE} from '@ngneat/transloco';
 import {Component, OnInit, ViewChild} from '@angular/core';
 import {SelectionModel} from '@angular/cdk/collections';
--- src/app/transloco-root.module.ts
+++ src/app/transloco-root.module.ts
@@ -27,6 +27,7 @@ export class TranslocoHttpLoader implements TranslocoLoader {
       useValue: translocoConfig({
         availableLangs: ['en', 'es'],
         defaultLang: 'en',
+        fallbackLang: 'en',
         // Remove this option if your application doesn't support changing language in runtime.
         reRenderOnLangChange: true,
         prodMode: environment.production,
