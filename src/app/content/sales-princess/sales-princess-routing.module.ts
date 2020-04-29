b821e2437a4fb7613f3efa3701a8c0512601b894
Alan Schmollgruber <alanschmollgruber@gmail.com>
2020-04-29 17:52:51 +0200

    SalesPrincess checkout of sold-items process.

--- src/app/content/sales-princess/sales-princess-routing.module.ts
+++ src/app/content/sales-princess/sales-princess-routing.module.ts
@@ -6,7 +6,7 @@ import {PageNotFoundComponent} from "../page-not-found/page-not-found.component"
 
 const routes: Routes = [
   {path: 'newdeliveryfromwarehouse', component: NewDeliveryFromWarehouseComponent},
-  {path: 'checkoutsolditemscomponent', component: CheckoutSoldItemsComponent}
+  {path: 'checkoutsolditems', component: CheckoutSoldItemsComponent}
 ];
 
 @NgModule({
