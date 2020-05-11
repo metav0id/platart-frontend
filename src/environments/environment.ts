34385ac64157a91acc3752a2c4f4a325830bf33c
Alan Schmollgruber <alanschmollgruber@gmail.com>
2020-05-11 16:23:11 +0200

    Environment variables fix.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -28,7 +28,6 @@ export const environment = {
   sendAllSoldItemsListURL:  'http://localhost:8081/shops/sendAllSoldItemsList',
   deleteCurrentSoldItemsListURL:  'http://localhost:8081/shops/deleteCurrentSoldItemsList',
   loadAllSoldItemsListURL: 'http://localhost:8081/shops/loadAllCurrentSoldItemsList',
-
 };
 
 /*
