a832175ce1702445c90739d3d3b356de71685fa9
Alan Schmollgruber <alanschmollgruber@gmail.com>
2020-05-11 13:21:42 +0200

    Checkout sold items.

--- src/app/content/sales-princess/sales-princess-routing.module.ts
+++ src/app/content/sales-princess/sales-princess-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {NewDeliveryFromWarehouseComponent} from './new-delivery-from-warehouse/new-delivery-from-warehouse.component';
 import {CheckoutSoldItemsComponent} from './checkout-sold-items/checkout-sold-items.component';
 import {PageNotFoundComponent} from "../page-not-found/page-not-found.component";
+import {ViewShopInventoryComponent} from "./view-shop-inventory/view-shop-inventory.component";
 
 const routes: Routes = [
   {path: 'newdeliveryfromwarehouse', component: NewDeliveryFromWarehouseComponent},
-  {path: 'checkoutsolditems', component: CheckoutSoldItemsComponent}
+  {path: 'checkoutsolditems', component: CheckoutSoldItemsComponent},
+  {path: 'viewshopinventory', component: ViewShopInventoryComponent}
 ];
 
 @NgModule({
--- src/app/content/sales-princess/sales-princess.module.ts
+++ src/app/content/sales-princess/sales-princess.module.ts
@@ -22,6 +22,7 @@ import { CheckoutSoldItemsDetailsComponent } from './checkout-sold-items/checkou
 import {MatCardModule} from "@angular/material/card";
 import {TranslocoRootModule} from "../../transloco-root.module";
 import { CheckoutSoldItemsSendVerificationComponent } from './checkout-sold-items/checkout-sold-items-send-verification/checkout-sold-items-send-verification.component';
+import { ViewShopInventoryComponent } from './view-shop-inventory/view-shop-inventory.component';
 
 
 @NgModule({
@@ -30,7 +31,8 @@ import { CheckoutSoldItemsSendVerificationComponent } from './checkout-sold-item
     NewDeliveryFromWarehouseDetailsComponent,
     CheckoutSoldItemsComponent,
     CheckoutSoldItemsDetailsComponent,
-    CheckoutSoldItemsSendVerificationComponent
+    CheckoutSoldItemsSendVerificationComponent,
+    ViewShopInventoryComponent
   ],
   imports: [
     CommonModule,
