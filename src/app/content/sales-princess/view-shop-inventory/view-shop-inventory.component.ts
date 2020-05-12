fc625eff4387d52e10fa24c3093c74c8df961fd2
alnbuber <alanschmollgruber@gmail.com>
2020-05-12 19:26:06 +0200

    View Shop inventory

--- src/app/content/sales-princess/view-shop-inventory/view-shop-inventory.component.ts
+++ src/app/content/sales-princess/view-shop-inventory/view-shop-inventory.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import {ShopInventoryItem} from './view-shop-inventory-DTOs/ShopInventoryItem';
+import {MatTable, MatTableDataSource} from '@angular/material/table';
+import {ViewShopInventoryService} from "./view-shop-inventory.service";
+import {observable} from "rxjs";
+import {FormControl, Validators} from "@angular/forms";
+import {Shop} from "../../commonDTOs/shop";
 
 @Component({
   selector: 'app-view-shop-inventory',
@@ -7,9 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewShopInventoryComponent implements OnInit {
 
-  constructor() { }
+  // table information
+  displayedColumns: string[] = ['position', 'shop', 'category', 'quantity', 'priceSalesPerUnit', 'priceListPerUnit' ];
+  dataSource = new MatTableDataSource();
+  @ViewChild('myShopsItemsTable') table: MatTable<any>;
+
+  /** Category selection */
+  public shopControl = new FormControl('', Validators.required);
+
+  /** List of available shops */
+  public listShops: Shop[] = [{name: 'shop1'}, {name: 'shop2'}];
+  public selectedShopToFilterOnList = '';
+
+  constructor(private viewShopInventoryService: ViewShopInventoryService) { }
 
   ngOnInit(): void {
+  }
+
+  // fetch inventory data
+  getShopItems(): void {
+    const shopItems: ShopInventoryItem[] = [];
+    this.viewShopInventoryService.getAllItemsObs(this.selectedShopToFilterOnList).subscribe((observable) => {
+      console.log(observable);
+      this.dataSource = new MatTableDataSource(observable);
+      this.table.renderRows();
+    });
+  }
+
+  getDeliveryItemList(shop: string) {
+    console.log('Loading data for ' + shop);
+    this.getShopItems();
+  }
+
+  // filter methods
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
   }
 
 }
---
+++ src/app/content/sales-princess/view-shop-inventory/view-shop-inventory.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {ShopInventoryItem} from './view-shop-inventory-DTOs/ShopInventoryItem';
+import {observable, Observable} from 'rxjs';
+import {ShopsStockItemDTO} from "./view-shop-inventory-DTOs/ShopsStockItemDTO";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ViewShopInventoryService {
+
+  constructor(private http: HttpClient) { }
+
+
+
+  getAllItemsObs(shopInput: string): Observable<ShopInventoryItem[]> {
+    const ELEMENT_DATA: ShopInventoryItem[] = [
+      {
+        position: 1,
+        shop: 'shop1',
+        category: 'pulsera',
+        quantity: 5,
+        priceSalesPerUnit: 40,
+        priceListPerUnit: 40
+      },
+    ];
+
+    const requestedShop: ShopsStockItemDTO = {
+      shop: shopInput
+    };
+    return this.http.post<ShopInventoryItem[]>('http://localhost:8081/shops/getShopInventoryItems', requestedShop);
+
+    /*const newObs = new Observable<ShopInventoryItem[]>((observable) => {
+      observable.next(ELEMENT_DATA);
+    });
+    return newObs;*/
+  }
+}
