2be09bdb8628634ff2dbf4aae6b150b27e783b4d
alnbuber <alanschmollgruber@gmail.com>
2020-05-13 11:15:36 +0200

    View Shop inventory - UI improvement

--- src/app/content/manager-king/manager-shops-info/manager-shops-info.component.ts
+++ src/app/content/manager-king/manager-shops-info/manager-shops-info.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {TRANSLOCO_SCOPE} from '@ngneat/transloco';
+import {FormControl, Validators} from "@angular/forms";
+import {Shop} from "../../commonDTOs/shop";
 
 @Component({
   selector: 'app-manage-shops-info',
@@ -9,9 +11,20 @@ import {TRANSLOCO_SCOPE} from '@ngneat/transloco';
 })
 export class ManagerShopsInfoComponent implements OnInit {
 
+  /** Category selection */
+  public shopControl = new FormControl('', Validators.required);
+
+  /** List of available shops */
+  public listShops: Shop[] = [{name: 'shop1'}, {name: 'shop2'}];
+  public selectedShopToFilterOnList = '';
+  public selectedShopToDisplay = '';
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getDeliveryItemList(selectedShopToFilterOnList: string) {
+    console.log(selectedShopToFilterOnList);
+  }
 }
