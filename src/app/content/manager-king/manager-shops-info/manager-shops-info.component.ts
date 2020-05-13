e2db19948f14cd95d353dcc651569f86161633de
alnbuber <alanschmollgruber@gmail.com>
2020-05-13 13:57:49 +0200

    Check Out Sold Items - RESTapi - feching of shops.

--- src/app/content/manager-king/manager-shops-info/manager-shops-info.component.ts
+++ src/app/content/manager-king/manager-shops-info/manager-shops-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {TRANSLOCO_SCOPE} from '@ngneat/transloco';
 import {Shop} from './manager-shops-info-DTOs/Shop';
 import {MatTable, MatTableDataSource} from '@angular/material/table';
