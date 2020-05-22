a8fc7b43df095b05edfa4695baae97f2c0e42937
enrico2507 <enrico.reitberger@googlemail.com>
2020-05-22 16:50:39 +0200

    implemented auto check for signed in user

--- src/app/content/services/auth.service.ts
+++ src/app/content/services/auth.service.ts
@@ -15,6 +15,19 @@ export class AuthService {
   constructor(public afs: AngularFirestore,
               public afAuth: AngularFireAuth,
               public router: Router) {
+    console.log('Reloading page...');
+    this.afAuth.authState.subscribe(user => {
+      if (user) {
+        this.getUserData(user.uid).subscribe(firestoreObj => {
+          localStorage.setItem('user', JSON.stringify(user));
+          localStorage.setItem('role', firestoreObj.role);
+          this.router.navigateByUrl('/home');
+        });
+      } else {
+        localStorage.setItem('user', null);
+        localStorage.setItem('role', null);
+      }
+    });
   }
 
   signIn(email, password) {
@@ -25,7 +38,8 @@ export class AuthService {
           localStorage.setItem('role', firestoreObj.role);
           observer.next();
         });
-      }).catch( error => {
+      }).catch(error => {
+        localStorage.setItem('user', null);
         localStorage.setItem('user', null);
         observer.error();
       });
