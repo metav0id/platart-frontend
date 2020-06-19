4f8b801550924dd5fda4eacfa636940aa8749bd1
Alan Schmollgruber <alanschmollgruber@gmail.com>
2020-06-19 12:07:17 +0200

    Chechout-Sold-Items component update: - Update all functionalities to be shop-dependant.

--- src/app/content/sales-princess/checkout-sold-items/checkout-sold-items.service.ts
+++ src/app/content/sales-princess/checkout-sold-items/checkout-sold-items.service.ts
@@ -6,6 +6,8 @@ import {observable, Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
 import {ShopsCheckoutSoldItemsDTO} from './checkout-sold-items-DTOs/ShopsCheckoutSoldItemsDTO';
 import {ShopDTO} from '../../warehouse-queen/new-delivery-to-shop/new-delivery-to-shop-DTOs/shop-dto';
+import {ShopNameDTO} from './checkout-sold-items-DTOs/Shop-name-DTO';
+import {SaveCheckoutSoldItemsDTO} from './checkout-sold-items-DTOs/SaveCheckoutSoldItemsDTO';
 
 @Injectable({
   providedIn: 'root'
@@ -19,16 +21,21 @@ export class CheckoutSoldItemsService {
     return this.http.post<WarehouseItemCategoryDTO[]>(environment.getAllCategories, null);
   }
 
-  // get all sold items
-  public getAllItemsAllShops(): void {
-    this.http.post<ShopsCurrentInventoryDTO[]>(environment.getAllShops, null).subscribe(JsonDto => {
-      console.log(JsonDto);
-    });
+  // get all sold items for a specfic shop
+  public getSpecificShopSoldItemsList(selectedShop: string): Observable<ShopsCheckoutSoldItemsDTO[]> {
+    const shopDTO: ShopNameDTO = {
+      shop: selectedShop
+    };
+    return this.http.post<null>(environment.getShopSpecificSoldItemsList, shopDTO);
   }
 
   // save all sold items
-  public getAllSoldItemsList(): Observable<ShopsCheckoutSoldItemsDTO[]> {
-    return this.http.post<null>(environment.getAllSoldItemsListURL, null);
+  public saveSpecificShopSoldItemsList(selectedShop: string, soldItemList: ShopsCheckoutSoldItemsDTO[]): Observable<null> {
+    const saveItemsDTO: SaveCheckoutSoldItemsDTO = {
+      shop: selectedShop,
+      itemsDTOList: soldItemList
+    };
+    return this.http.post<null>(environment.saveAllSoldItemsListURL, saveItemsDTO);
   }
 
   // save all sold items
@@ -36,14 +43,29 @@ export class CheckoutSoldItemsService {
     return this.http.post<null>(environment.saveAllSoldItemsListURL, soldItemList);
   }
 
+  // send shop specific sold items
+  public sendSpecificShopSoldItemsList(
+    selectedShop: string,
+    soldItemList: ShopsCheckoutSoldItemsDTO[]): Observable<ShopsCheckoutSoldItemsDTO[]> {
+    const saveItemsDTO: SaveCheckoutSoldItemsDTO = {
+      shop: selectedShop,
+      itemsDTOList: soldItemList
+    };
+    return this.http.post<null>(environment.sendAllSoldItemsListURL, saveItemsDTO);
+  }
+
   // send all sold items
   public sendAllSoldItemsList(soldItemList: ShopsCheckoutSoldItemsDTO[]): Observable<ShopsCheckoutSoldItemsDTO[]> {
     return this.http.post<null>(environment.sendAllSoldItemsListURL, soldItemList);
   }
 
-  // delete current sold items list
-  public deleteCurrentSoldItemsList(): Observable<null> {
-    return this.http.post<null>(environment.deleteCurrentSoldItemsListURL, null);
+  // TODO: update URL
+  // delete shop specific checkout sold items list
+  public deleteShopSpecificCheckoutSoldItemsList(shopSelected: string): Observable<null> {
+    const shopDTO: ShopNameDTO = {
+      shop: shopSelected
+    };
+    return this.http.post<null>(environment.deleteCurrentSoldItemsListURL, shopDTO);
   }
 
   public loadCurrentSoldItemsList(): Observable<ShopsCheckoutSoldItemsDTO[]> {
