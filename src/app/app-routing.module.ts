880996f3bfbfce9fac0cbce2b85a0fcf9b77c531
Alan Schmollgruber <alanschmollgruber@gmail.com>
2020-06-25 12:31:22 +0200

    Routing improvement

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {LoginComponent} from './content/pages/login/login.component';
 import {AuthGuard} from './content/guards/auth.guard';
-
-import {LandingPageWarehouseComponent} from "./content/landing-page/landing-page-warehouse.component";
-
+import {LandingPageWarehouseComponent} from './content/landing-page/landing-page-warehouse.component';
 
 const routes: Routes = [
   { path: 'login'   , component: LoginComponent },
   { path: 'landingPage' , component: LandingPageWarehouseComponent, canActivate: [AuthGuard]},
-  { path: '**', redirectTo: 'landingPage' },
+  { path: '**', redirectTo: 'login' },
 ];
 
 @NgModule({
